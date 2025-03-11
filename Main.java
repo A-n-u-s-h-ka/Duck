41c0e600116468d47f720f73e780e536d2c46903
A-n-u-s-h-ka <165628531+A-n-u-s-h-ka@users.noreply.github.com>
2025-03-11 20:47:14 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+Name: Anushka Chatterjee
+PRN: 23070126015
+Batch: AIML A1
+ */
+public class Main {
+    public static void main(String[] args) {
+        RubberDuck rb = new RubberDuck();
+        System.out.println("------------");
+        rb.display();
+        rb.performQuack();
+        rb.performSwim();
+        rb.performFly();
+        System.out.println("------------");
+        MallardDuck md = new MallardDuck();
+        System.out.println("------------");
+        md.display();
+        md.performQuack();
+        md.performSwim();
+        md.performFly();
+        System.out.println("------------");
+
+
+        }
+    }
